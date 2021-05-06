663a845ae1e4e3ce11e11a4c73c2da7f0dc58894
kndrckgmz <kendrickdgomez@gmail.com>
2021-05-06 14:52:33 +0530

    loggin added

--- src/App.js
+++ src/App.js
@@ -14,81 +14,24 @@ import Testing from './Page/Testing';
 import Widget from './Page/res/widget';
 import Donor from './Page/res/donor';
 import Login from './Login';
-import fire from './Page/res/fire_config';
+
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function Main() {
 
-  const [user, setUser] = useState(true);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-
-  const clearInputs = () =>{
-    setEmail("");
-    setPassword("");
-  } 
-
-  const clearErrors = () =>{
-    setEmailError("");
-    setPasswordError("");
-  } 
-
-  const handleLogin = () => {
-    clearErrors();
-    fire
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .catch((err) => {
-      switch(err.code){
-        case "auth/invalid-email":
-        case "auth/user-disabled":
-        case "auth/user-not-found":
-          setEmailError(err.message);
-          break;
-        case "auth/wrong-password":
-          setPasswordError(err.message);
-          break;  
-      }
-    });
-  }
-
-  const handleSignUp = () => {
-    clearErrors();
-    fire
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .catch((err) => {
-      switch(err.code){
-        case "auth/email-already-in-use":
-        case "auth/invalid-email":
-          setEmailError(err.message);
-          break;
-        case "auth/weak-password":
-          setPasswordError(err.message);
-          break;  
-      }
-    });
-  }
-
-  const handleLogOut = () => {
-    fire.auth().signOut();
-  }
+  const [logincheck, setLoginCheck] = useState(false);
+  const [user, setUser] = useState(false);
 
   useEffect(()=>{
-      fire.auth().onAuthStateChanged((user) =>{
-        if(user)
-        {
-          clearInputs();
-          setUser(true);
-        }
-        else
-        {
-          setUser(false);
-        }
-      });
-  },[]);
+    if(logincheck!==false)
+    {
+      setUser(true);
+    }
+    else
+    {
+      setUser(false);
+    }
+  },[logincheck]);
 
   return (
     <>
@@ -103,14 +46,8 @@ function Main() {
       </div>
       <Switch>
         <Route path="/Login" component={props => (<Login {...props} 
-          email={email}
-          setEmail={setEmail}
-          emailError={emailError}
-          password={password}
-          setPassword={setPassword}
-          passwordError={passwordError}
-          handleLogin={handleLogin}
-          handleLogOut={handleLogOut}
+        logincheck={logincheck}
+        setLoginCheck={setLoginCheck}
         />)}></Route>
         <Route path="/" exact component={props => (<Ambulance {...props} user={user}/>)}></Route>
         <Route path="/Beds" component={props => (<Beds {...props} user={user}/>)}></Route>
--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,86 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
+import fire from './Page/res/fire_config';
 
-const Login = ({
-  email,
-  setEmail,
-  emailError,
-  password,
-  setPassword,
-  passwordError,
-  handleLogin,
-  handleLogOut
-}) => {
+const Login = ({logincheck, setLoginCheck}) => {
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleLogin = () => {
+    clearErrors();
+    fire
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch((err) => {
+      if(err.code==="auth/invalid-email"||err.code==="auth/user-disabled"||err.code==="auth/user-not-found")
+    { 
+      let e = document.getElementById("errore");
+      e.style.opacity=1;
+      setEmailError(err.message);
+      setTimeout(()=>{
+        setEmailError("");
+        e.style.opacity=0;
+      },10000);
+    }
+    else if(err.code==="auth/wrong-password")
+    {
+      let e = document.getElementById("errorp");
+      e.style.opacity=1;
+      setPasswordError(err.message);
+      setTimeout(()=>{
+        setPasswordError("");
+        e.style.opacity=0;
+      },10000);
+    }
+    });
+  }
+
+
+  // switch(err.code){
+  //   case "auth/invalid-email":
+  //   case "auth/user-disabled":
+  //   case "auth/user-not-found":
+  //     setEmailError(err.message);
+  //     break;
+  //   case "auth/wrong-password":
+  //     setPasswordError(err.message);
+  //     break;  
+  // }
+
+
+  const clearInputs = () =>{
+    setEmail("");
+    setPassword("");
+  } 
+
+  const clearErrors = () =>{
+    setEmailError("");
+    setPasswordError("");
+  } 
+
+  const handleLogOut = () => {
+    fire.auth().signOut();
+  }
+
+  const userListener = () => {
+    fire.auth().onAuthStateChanged((logincheck) =>{
+      if(logincheck)
+      {
+        clearInputs();
+        setLoginCheck(logincheck);
+      }
+      else
+      {
+        setLoginCheck(false);
+      }
+    });
+  }
+
+  useEffect(()=>{
+    userListener();
+  },[]);
 
     function showpass() {
         var x = document.getElementById("pass");
@@ -22,37 +92,57 @@ const Login = ({
       }
 
     return (
-    <div className="content">
+      <>
+    {logincheck ?
+      (
+        <div className="content">
         <div className="grid">
         <div className="login">
             <div className="sv"></div>            
 
-            <div className="label" id="signin">Sign In</div>
+            <div className="login-title" id="signin">YOU ARE LOGGED IN !</div>
+            <button className="log" onClick={handleLogOut}>
+            LOGOUT
+            </button> 
+        </div>
+        </div>
+        </div>
+      )
+      :
+      (
+        <div className="content">
+        <div className="grid">
+        <div className="login">
+            <div className="sv"></div>            
 
-            <input className="input" type="email" id="name" placeholder="Username"
+            <div className="login-title" id="signin">ADMINISTRATOR LOGIN</div>
+
+            <input className="login-input" type="email" id="name" placeholder="Email"
             required 
             value={email} 
             onChange={(e)=>setEmail(e.target.value)}></input>
-            <p className="logerror" id="error">{emailError}</p> 
+            <p className="logerror" id="errore">{emailError}</p> 
 
-            <input className="input" type="password" id="pass" placeholder="Password"
+            <input className="login-input" type="password" id="pass" placeholder="Password"
             required
             value={password} 
             onChange={(e)=>setPassword(e.target.value)}></input>
-            <p className="logerror" id="error">{passwordError}</p>
+            <p className="logerror" id="errorp">{passwordError}</p>
 
-            <label className="label">
-              Show Password
-            <input className="checkbox1" type="checkbox" onClick={showpass}></input>
-            </label>
-              
-            <button className="log" onClick={handleLogin}>Login</button>
+            <div className="login-flex">
+              <label className="password-check-label">Show Password</label>
+              <input className="checkbox" type="checkbox" id="passwordcheck" onClick={showpass}/>
+              <label htmlFor="passwordcheck" className="switch"/>      
+            </div>  
+            <button className="log" onClick={handleLogin}>LOGIN</button>
         </div>
-        <button className="logout" onClick={handleLogOut}>
-            LOGOUT
-          </button> 
         </div>
-    </div>
+        </div>
+      )
+
+    }
+    </>
+    
     )
 }
 
